3b0b4c391e0f0be9e5dfac3daba56d794927adb1
Micah Walter <micah@micahwalter.com>
2013-12-02 20:09:57 -0500

    set up routing and templating

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
-var express = require("express");
-var app = express();
+var express = require('express')
+	, cons = require('consolidate')
+	, site = require('./site')
+	, app = express();
+
+// Config 
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+// General pages
+app.get('/', site.index);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
